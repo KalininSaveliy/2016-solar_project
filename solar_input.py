51f0f3b7fbbe28d502acffb32a8e26176bd0cef6
PioterM <pioter.mehraliev@yandex.ru>
2019-11-13 12:12:43 +0300

    написал функцию write_space_objects_data_to_file

--- solar_input.py
+++ solar_input.py
@@ -19,7 +19,7 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIX: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
@@ -97,8 +97,11 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIX: should store real values
+            argu = (type(space_objects), space_objects.R, space_objects.color,
+                    space_objects.m, space_objects.x, space_objects.y,
+                    space_objects.Vx, space_objects.Vy)
+            print(out_file, "%s %f %s %f %f %f %f" % argu)
+
 
 # FIX: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
