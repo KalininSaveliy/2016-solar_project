208c66c2a59e3bdc0bd2f0e0d9d360cf04320802
PioterM <pioter.mehraliev@yandex.ru>
2019-11-13 10:43:28 +0300

    добавил формулы Fx и Fy

--- solar_model.py
+++ solar_model.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # license: GPLv3
+import math
 
 gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
@@ -19,8 +20,11 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        corner = atan(body.y - obj.y/body.x - obj.x)
+        body.Fx += 1  # FIXME: нужно вывести формулу...#исправлено
+        Fx = gravitational_constant*body.m*obj.m*math.cos(corner)/r**2
+        body.Fy += 2  # FIXME: нужно вывести формулу...№исправлено
+        Fy = gravitational_constant*body.m*obj.m*math.sin(corner)/r**2
 
 
 def move_space_object(body, dt):
